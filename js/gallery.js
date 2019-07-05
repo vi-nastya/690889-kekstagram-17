005c600c3101f6d07f3e6365b7629e98f1876fbd
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-07-05 07:51:06 -0700

    Added missing file

---
+++ js/gallery.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function () {
+  var successHandler = function (data) {
+    window.render(data);
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.load(successHandler, errorHandler);
+})();
