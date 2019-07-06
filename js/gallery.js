96910f7614a390fb21d7dbf7bf944de701c37690
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-07-06 08:45:03 -0700

    Show filters after successful load

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,18 @@
 'use strict';
 
 (function () {
+  var filters = document.querySelector('.img-filters');
+
   var successHandler = function (data) {
     window.render(data);
+
+    filters.classList.remove('img-filters--inactive');
   };
 
   var errorHandler = function (errorMessage) {
     var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style =
+      'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
     node.style.position = 'absolute';
     node.style.left = 0;
     node.style.right = 0;
