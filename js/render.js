b30242c23bcd2528c6ab2c15b18c951c7a77747a
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-07-09 11:22:58 -0700

    Added show more comments

--- js/render.js
+++ js/render.js
@@ -36,20 +36,24 @@
 
   };
 
-  window.renderBigPicture = function (pictureToShow) {
-    var bigPicture = document.querySelector('.big-picture');
-    // add url
-    bigPicture.querySelector('.big-picture__img img').setAttribute('src', pictureToShow.url);
-    // add likes
-    bigPicture.querySelector('.likes-count').textContent = pictureToShow.likes;
-    // add comments counter
-    bigPicture.querySelector('.comments-count').textContent = pictureToShow.comments.length;
-    bigPicture.querySelector('.social__caption').textContent = pictureToShow.description;
 
-    // add comments
+  var getCommentsToRender = function (comments, numShown) {
+    var numCommentsToRender = 0;
+    if (numShown < comments.length) {
+      if (comments.length - numShown < 5) {
+        numCommentsToRender = comments.length - numShown;
+      } else {
+        numCommentsToRender = 5;
+      }
+      return comments.slice(numShown, numShown + numCommentsToRender);
+    } else {
+      return null;
+    }
+  };
+
+  var renderComments = function (comments) {
     var fragment = document.createDocumentFragment();
-
-    pictureToShow.comments.forEach(function (currentComment) {
+    comments.forEach(function (currentComment) {
       var comment = document.createElement('li');
       comment.classList.add('social__comment');
 
@@ -66,22 +70,68 @@
 
       comment.appendChild(avatar);
       comment.appendChild(commentText);
-
       fragment.appendChild(comment);
-
     });
+    return fragment;
+  };
 
-    bigPicture.querySelector('.social__comments').appendChild(fragment);
+  window.renderBigPicture = function (pictureToShow) {
+    var bigPicture = document.querySelector('.big-picture');
+    // add url
+    bigPicture.querySelector('.big-picture__img img').setAttribute('src', pictureToShow.url);
+    // add likes
+    bigPicture.querySelector('.likes-count').textContent = pictureToShow.likes;
+    // add comments counter
+    bigPicture.querySelector('.comments-count').textContent = pictureToShow.comments.length;
+    bigPicture.querySelector('.social__caption').textContent = pictureToShow.description;
+
+    var commentsSection = bigPicture.querySelector('.social__comments');
+    var showMoreCommentsButton = bigPicture.querySelector('.comments-loader');
+    showMoreCommentsButton.classList.remove('visually-hidden');
+    var shownCommentsCounter = bigPicture.querySelector('.comments-count-shown');
+
+    // add comments
+    var hideMoreCommentsButton = function () {
+      showMoreCommentsButton.classList.add('visually-hidden');
+    };
+
+    var shownComments = 0;
+    var renderedComments = null;
+    if (pictureToShow.comments.length <= 5) {
+      renderedComments = renderComments(pictureToShow.comments);
+      shownComments = pictureToShow.comments.length;
+      hideMoreCommentsButton();
+    } else {
+      renderedComments = renderComments(pictureToShow.comments.slice(0, 5));
+      shownComments = 5;
+    }
+    shownCommentsCounter.textContent = shownComments;
+    commentsSection.appendChild(renderedComments);
+
+    // показать следующие 5 комментариев
+    var onMoreCommentsClick = function () {
+      var newCommentsToRender = getCommentsToRender(pictureToShow.comments, shownComments);
+      if (newCommentsToRender) {
+        renderedComments = renderComments(newCommentsToRender);
+        shownComments += newCommentsToRender.length;
+        commentsSection.appendChild(renderedComments);
+        shownCommentsCounter.textContent = Number(shownCommentsCounter.textContent) + newCommentsToRender.length;
+        if (shownComments === pictureToShow.comments.length) {
+          hideMoreCommentsButton();
+        }
+      }
+    };
+
+    showMoreCommentsButton.addEventListener('click', onMoreCommentsClick);
 
 
-    bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden');
-    bigPicture.querySelector('.comments-loader').classList.add('visually-hidden');
     bigPicture.classList.remove('hidden');
 
     var closePicture = bigPicture.querySelector('.big-picture__cancel');
     closePicture.addEventListener('click', function (evt) {
       evt.preventDefault();
       bigPicture.classList.add('hidden');
+      commentsSection.innerHTML = '';
     });
   };
 
